67df9ec1b86b47a55304886546e58a5cea9950be
Christian <kukruchristian@gmail.com>
2023-07-29 17:42:57 +0100

    updated the app design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 export default function App() {
   const [input, setInput] = useState('');
   const [todo, setTodo] = useState(() => {
     const localValue = localStorage.getItem('ITEMS')
     if (localValue == null) return []
-    
+
     return JSON.parse(localValue)
   })
 
@@ -16,14 +17,16 @@ export default function App() {
 
   function handleSubmit(e) {
     e.preventDefault()
-    setTodo(currentTodo => { return [...currentTodo, { id: crypto.randomUUID(), title: input, completed: false }] })
+    if(input !== ""){
+    setTodo(currentTodo => { return [...currentTodo, { id: crypto.randomUUID(), title: input, completed: false, duration: null }] })
+    }
     setInput('')
   }
   function toggleTodo(id, completed) {
     setTodo(currentTodo => {
       return currentTodo.map(todo => {
         if (todo.id === id) {
-          return { ...todo, completed }
+          return { ...todo, completed}
         }
         return todo;
       })
@@ -38,24 +41,35 @@ export default function App() {
   }
   return (
     <>
-      <form onSubmit={handleSubmit} className='newForm'>
-        <div className='formRow'>
-          <label htmlFor='item'> New Item </label>
-          <input value={input} onChange={e => { setInput(e.target.value) }} type='text' id='item' />
-        </div>
-        <button className='btn'>Add</button>
-      </form>
-      <h1>Todo List</h1>
+    <h1>TODO-LIST APP</h1>
+      <div className='formContainer'>
+        <form onSubmit={handleSubmit} className='newForm'>
+          <div className='formRow'>
+            <label htmlFor='item'> What Are The Task For Today ? </label>
+            <input className='inputField' value={input} onChange={e => { setInput(e.target.value) }} type='text' id='item' placeholder='Enter a task here' />
+          </div>
+          <button className='btn'>Add Task</button>
+        </form>
+      </div>
+      <h2>List</h2>
       <ul className='list'>
         {todo.length === 0 && 'No Todo'}
         {todo.map(todo => {
-          return (<li key={todo.id}>
-            <label>
-              <input type='checkbox' checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
-              {todo.title}
-            </label>
-            <button onClick={() => deleteTodo(todo.id)} className='delBtn'>Delete</button>
-          </li>
+          return (
+            <li key={todo.id}>
+              <div className='todoItems'>
+                <label>
+                  <input type='checkbox' checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                  <span style={todo.completed? {textDecoration:'line-through'} : {} }>
+                  {todo.title}
+                  </span>
+                </label>
+                <button onClick={() => deleteTodo(todo.id)} className='delBtn'>
+                  <span> <FontAwesomeIcon icon={faTrash} /> </span>
+                  <span> Delete </span>
+                </button>
+              </div>
+            </li>
           )
         })}
       </ul>
